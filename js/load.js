bbe18afb0183339252934c7c3256cf213d6d82f2
Nimoscmp <nimos.cmp@gmail.com>
2021-02-05 19:29:09 -0500

    Agregar js para preload

---
+++ js/load.js
@@ -0,0 +1,10 @@
+(function () {
+
+    //Preload behaviour
+    var preload_window = document.getElementById('preloadWindow')
+
+    window.addEventListener('load', () => {
+        preload_window.remove()
+    })
+
+})();    
