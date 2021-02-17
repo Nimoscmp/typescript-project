271b9b833f241fcb7fd3005c806151101967a628
nimos <nimos.cmp@gmail.com>
2021-02-17 18:50:52 -0500

    Modificar main

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log('hola mkey');
+/*=================================
+Typescript/Javascript file
+Developed by Nabalas©
+2021
+
+
+================================= */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.errortext = exports.sign_in_button = void 0;
+var users_1 = require("./users");
+var login_1 = __importDefault(require("./login"));
+exports.sign_in_button = document.getElementById('signInButton');
+var password_input = document.getElementById('passwordInput');
+exports.errortext = document.getElementById('errorID');
+exports.errortext.classList.add('d-none');
+password_input.addEventListener('focusout', function () {
+    var email = document.getElementById('emailInput').value;
+    var username = document.getElementById('userInput').value;
+    var password = document.getElementById('passwordInput').value;
+    login_1.default(users_1.getUsers()[0], { email: email, username: username, password: password });
+});
--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,26 @@
-console.log('hola mkey');
+/*=================================
+Typescript/Javascript file
+Developed by Nabalas©
+2021
+
+
+================================= */
+
+import {getUsers} from './users';
+import login from './login';
+
+export let sign_in_button = document.getElementById('signInButton')!;
+let password_input = document.getElementById('passwordInput')!;
+export let errortext = document.getElementById('errorID');
+
+errortext!.classList.add('d-none');
+
+password_input.addEventListener('focusout', () => {
+
+    let email = (<HTMLInputElement>document.getElementById('emailInput')).value;
+    let username = (<HTMLInputElement>document.getElementById('userInput')).value;
+    let password = (<HTMLInputElement>document.getElementById('passwordInput')).value;
+
+    login(getUsers()[0], {email, username, password});
+
+})
